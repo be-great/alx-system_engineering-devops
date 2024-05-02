4b8063e94e884dde571ba8de57c1dec5bd2a0a94
be-great <begreat5333@gmail.com>
2024-05-02 14:29:07 +0300

    delete nessessory files

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/hbtt{1,4}n/).join
--- 0x06-regular_expressions/100-textme.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-Sender = ARGV[0].scan(/from:(.*?)\]/)
-Receive = ARGV[0].scan(/to:(.*?)\]/)
-Flag = ARGV[0].scan(/flags:(.*?)\]/)
-puts [Sender, Receive, Flag].join(',')
