c928c9fd2d4183bbe665d97c4ae860348db57226
be-great <begreat5333@gmail.com>
2024-06-29 02:38:14 +0300

    update task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
-# Using Puppet, install flask from pip3.
-
-package { 'flask':
-  ensure => '2.1.0',
-  provider => 'pip3',
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
