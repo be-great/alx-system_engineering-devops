b6a030c579d8a25d1eb0bb60ed40752a8ebe0d68
be-great <begreat5333@gmail.com>
2024-06-29 05:39:51 +0300

    df

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
-#!/usr/bin/pup
-# create a file in /tmp
-
-package { 'flask':
+##!/usr/bin/pup
+# Using Puppt to install flask from pip3
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
