f1b42a2e968a6952bfdc49344d27b21ce8a50c49
be-great <begreat5333@gmail.com>
2024-06-29 03:26:09 +0300

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 # Using Puppet, create a file in /tmp.
 
 file { '/tmp/school':
-  mod     => '0744',
+  mode     => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 # Using Puppet, install flask from pip3.
 
-package { 'flask':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
