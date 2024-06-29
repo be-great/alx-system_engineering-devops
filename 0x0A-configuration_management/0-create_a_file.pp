b0f1490baa2fef05c95b3e7fe7ff7b39700ad6aa
be-great <begreat5333@gmail.com>
2024-06-29 03:30:00 +0300

    update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Using Puppet, create a file in /tmp.
 
 file { '/tmp/school':
-  mode     => '0744',
+  content =>'I love Puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
