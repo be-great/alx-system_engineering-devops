5376967ef4b54b4b89adbe14ec9c04bf621a419f
be-great <begreat5333@gmail.com>
2024-06-30 10:04:08 +0300

    update test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,43 +1,13 @@
-# 100-puppet_ssh_config.pp
-# Setting up my client config file
+# using Puppet to make changes to our configuration file
 
-include stdlib
-
-file { '/home/ubuntu/.ssh/config':
-  ensure => file,
-  owner  => 'ubuntu',
-  group  => 'ubuntu',
-  mode   => '0600',
-}
-
-file_line { 'Turn off passwd auth':
+file { '/etc/ssh/ssh_config':
   ensure  => present,
-  path    => '/home/ubuntu/.ssh/config',
-  line    => '    PasswordAuthentication no',
-  match   => '^\s*PasswordAuthentication',
-  replace => true,
-}
-
-file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '/home/ubuntu/.ssh/config',
-  line    => '    IdentityFile ~/.ssh/school',
-  match   => '^\s*IdentityFile',
-  replace => true,
-}
-
-file_line { 'Preferred auth method':
-  ensure  => present,
-  path    => '/home/ubuntu/.ssh/config',
-  line    => '    PreferredAuthentications publickey',
-  match   => '^\s*PreferredAuthentications',
-  replace => true,
-}
-
-file_line { 'Host configuration':
-  ensure  => present,
-  path    => '/home/ubuntu/.ssh/config',
-  line    => 'Host your_server_ip_or_hostname',
-  match   => 'Host ',
-  replace => true,
+  content => "# Your machine has an SSH configuration
+Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+",
+  mode    => "0744",
+  owner   => "test",
+  group   => "test",
 }
