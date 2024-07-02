92ed0de2a2eac16043b3afbe8e6c65c39e5e63cc
be-great <begreat5333@gmail.com>
2024-07-02 20:26:22 +0300

    tsk 7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,26 @@
+#  Install Nginx web server (w/ Puppet) 
+
+exec { 'apt update':
+  command => '/usr/bin/apt-get update -y'
+
+}
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['apt update'],
+}
+
+# Create a custom index.html file with "Hello World!"
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+exec { 'redirect_me':
+  command => 'sed -i '24i\    rewrite ^/redirect_me https://example.com permanent;' /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+service { 'nginx':
+  ensure => 'running',
+  require => package['nginx'],
+
+}
