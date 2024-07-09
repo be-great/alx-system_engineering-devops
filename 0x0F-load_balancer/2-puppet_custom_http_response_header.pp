27f91e572394293e36cc41d9d7b3af032a983055
be-great <begreat5333@gmail.com>
2024-07-10 01:05:37 +0300

    update task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,24 +1,23 @@
-# Add a custom HTTP header with Puppet 
-exec { "update":
+# Add a custom HTTP header with Puppet
+exec { 'update':
   provider => shell,
   command  => 'sudo apt-get -y update',
-  before   =>  Exec['nginx'],
+  before   => Exec['install nginx'],
 }
 
-exec { "nginx":
+exec { 'install nginx':
   provider => shell,
   command  => 'sudo apt-get install -y nginx',
-  before   =>  Exec['nginx config'],
+  before   => Exec['nginx config'],
 }
 
-exec { "nginx config":
+exec { 'nginx config':
   provider => shell,
-  command  => 'printf "%s\n\tadd_header X-Served-By \\"$HOST\\";" "$(grep -v "add_header X-Served-By" /etc/nginx/nginx.conf)" > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/nginx.conf',
-  before   =>  Exec['restart'],
+  command  => 'printf "%s\n\tadd_header X-Served-By \\"$HOST\\";" "$(grep -v "add_header X-Served-By" /etc/nginx/nginx.conf)" > /tmp/nginx.conf && sudo mv /tmp/nginx.conf /etc/nginx/nginx.conf',
+  before   => Exec['restart nginx'],
 }
 
-
-exec { "restart":
+exec { 'restart nginx':
   provider => shell,
-  command  => 'sudo service nginx restart'
+  command  => 'sudo service nginx restart',
 }
