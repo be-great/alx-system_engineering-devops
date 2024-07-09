5d1bce67f1ae67805be2f92a5f7e666660db4353
be-great <begreat5333@gmail.com>
2024-07-10 01:02:55 +0300

    update task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,19 +1,20 @@
+# Add a custom HTTP header with Puppet 
 exec { "update":
   provider => shell,
-  command  => 'sudo apt-get -y update'
-  before   =>  Exec['nginx']
+  command  => 'sudo apt-get -y update',
+  before   =>  Exec['nginx'],
 }
 
 exec { "nginx":
   provider => shell,
-  command  => 'sudo apt-get install -y nginx'
-  before   =>  Exec['nginx config']
+  command  => 'sudo apt-get install -y nginx',
+  before   =>  Exec['nginx config'],
 }
 
 exec { "nginx config":
   provider => shell,
   command  => 'printf "%s\n\tadd_header X-Served-By \\"$HOST\\";" "$(grep -v "add_header X-Served-By" /etc/nginx/nginx.conf)" > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/nginx.conf',
-  before   =>  Exec['restart']
+  before   =>  Exec['restart'],
 }
 
 
