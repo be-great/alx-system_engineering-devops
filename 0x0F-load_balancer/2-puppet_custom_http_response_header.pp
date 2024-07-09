ff5ff426319d5669bfd35a1a54786a467caaa87e
be-great <begreat5333@gmail.com>
2024-07-10 00:42:11 +0300

    task 3

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,23 @@
+exec { "update":
+  provider => shell,
+  command  => 'sudo apt-get -y update'
+  before   =>  Exec['nginx']
+}
+
+exec { "nginx":
+  provider => shell,
+  command  => 'sudo apt-get install -y nginx'
+  before   =>  Exec['nginx config']
+}
+
+exec { "nginx config":
+  provider => shell,
+  command  => 'printf "%s\n\tadd_header X-Served-By \\"$HOST\\";" "$(grep -v "add_header X-Served-By" /etc/nginx/nginx.conf)" > /tmp/nginx.conf && mv /tmp/nginx.conf /etc/nginx/nginx.conf',
+  before   =>  Exec['restart']
+}
+
+
+exec { "restart":
+  provider => shell,
+  command  => 'sudo service nginx restart'
+}
