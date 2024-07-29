8d4b96dee7baf59cb3774ee6f64aa23a19586785
be-great <begreat5333@gmail.com>
2024-07-29 15:17:58 +0300

    fdf

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,13 +7,13 @@ import re
 import requests
 import sys
 
-REST_API = "https://jsonplaceholder.typicode.com"
+URL = "https://jsonplaceholder.typicode.com"
 
 
 def get_employee_data(employee_id):
     """Fetch employee and todo data from API."""
-    user_url = f'{REST_API}/users/{employee_id}'
-    todos_url = f'{REST_API}/todos?userId={employee_id}'
+    user_url = f'{URL}/users/{employee_id}'
+    todos_url = f'{URL}/todos?userId={employee_id}'
 
     user_resp = requests.get(user_url)
     todos_resp = requests.get(todos_url)
@@ -41,16 +41,12 @@ def display_todo_progress(employee_id):
 
     print('Employee {} is done with tasks({}/{}):'.format(
         user_name, number_of_done_tasks, total_tasks))
-    for idx, task in enumerate(done_tasks, 1):
-        print('Task {} Formatting: OK'.format(idx))
+    for task in done_tasks:
+        print('\t {}'.format(task.get('title')))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        sys.exit(1)
-
-    if not re.fullmatch(r'\d+', sys.argv[1]):
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    display_todo_progress(employee_id)
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            employee_id = int(sys.argv[1])
+            display_todo_progress(employee_id)
