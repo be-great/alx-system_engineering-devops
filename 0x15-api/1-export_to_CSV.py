1bc47d52382cf5a75f45f1daddda6a634fc06546
be-great <begreat5333@gmail.com>
2024-07-29 17:42:26 +0300

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,27 +18,9 @@ def get_employee_data(employee_id):
 
     user_resp = requests.get(user_url)
     todos_resp = requests.get(todos_url)
-
-    if user_resp.status_code != 200 or todos_resp.status_code != 200:
-        return None
-
     user_data = user_resp.json()
     todos_data = todos_resp.json()
-
-    return user_data, todos_data
-
-
-def display_todo_progress(employee_id):
-    """Display the todo progress of the employee."""
-    data = get_employee_data(employee_id)
-    if data is None:
-        return
-
-    user_data, todos_data = data
     user_name = user_data.get('name')
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task.get('completed')]
-    number_of_done_tasks = len(done_tasks)
     export_to_csv(employee_id, user_name, todos_data)
 
 
@@ -62,4 +44,4 @@ if __name__ == '__main__':
         sys.exit(1)
 
     employee_id = int(sys.argv[1])
-    display_todo_progress(employee_id)
+    get_employee_data(employee_id)
