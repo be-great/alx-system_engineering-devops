f883f8d97d248ab9749a5ec7b543f6acd739bfb8
be-great <begreat5333@gmail.com>
2024-07-29 17:31:05 +0300

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -12,23 +12,22 @@ URL = "https://jsonplaceholder.typicode.com/users"
 
 def export_to_csv(employee_id, user_name, todos_data):
     """Export the todo data to a CSV file."""
-    with open('{}.csv'.format(employee_id), 'w') as csvfile:
-        for task in todos_data:
-            completed = task.get('completed')
-            """Complete"""
-            title_task = task.get('title')
-            """Done"""
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                employee_id, user_name, completed, title_task))
 
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    url_user = 'https://jsonplaceholder.typicode.com/users/' + employee_id
-    res = requests.get(url_user)
-    """ANYTHING"""
-    user_name = res.json().get('username')
-    task = url_user + '/todos'
-    res = requests.get(task)
-    todos_data = res.json()
-    export_to_csv(employee_id, user_name, todos_data)
+    if len(sys.argv) > 1:
+        employee_id = int(sys.argv[1])
+        user_url = f'{URL}/{employee_id}'
+        todos_url = f'{URL}/{employee_id}/todos'
+        user_resp = requests.get(user_url)
+        todos_resp = requests.get(todos_url)
+
+        user_name = user_resp.json().get('name')
+        todos_data = todos_resp.json()
+        filename = f'{employee_id}.csv'
+        with open(filename, 'w') as file:
+            for task in todos_data:
+                completed = task.get('completed')
+                title = task.get('title')
+                file.write('"{}","{}","{}","{}"\n'.format(
+                    employee_id, user_name, completed, title))
