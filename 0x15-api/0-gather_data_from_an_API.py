f329c14b4ac6aba1ff15321096282a89b0a035c3
be-great <begreat5333@gmail.com>
2024-07-29 16:05:32 +0300

    dfdsf

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,27 +8,23 @@ import requests
 import sys
 
 
+REST_API = "https://jsonplaceholder.typicode.com"
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
-            URL = "https://jsonplaceholder.typicode.com/"
-            employee_id = int(sys.argv[1])
-            user_url = f'{URL}users/{employee_id}'
-            todo_url = f'{URL}todos?userId={employee_id}'
-            user_data = requests.get(user_url).json()
-            todo_data = requests.get(todo_url).json()
-            name = user_data.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == employee_id,
-                                todo_data))
-            done_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            id = int(sys.argv[1])
+            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
             print(
                 'Employee {} is done with tasks({}/{}):'.format(
-                    name,
-                    len(done_tasks),
-                    len(tasks),
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
                 )
             )
-
-            if len(done_tasks) > 0:
-                for task in done_tasks:
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
                     print('\t {}'.format(task.get('title')))
