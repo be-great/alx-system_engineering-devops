b04ba6e1bbb898a90b8743a0535377ced201487b
be-great <begreat5333@gmail.com>
2024-08-14 05:32:45 +0300

    TASK 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""
-How many subs
-nd returns the number of subscribers
-for a given subreddit
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    return the number of subscribers
-    """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Mozilla/5.0"}
-    resp = requests.get(url, headers=headers, allow_redirects=False)
-
-    if resp.status_code == 200:
-        data = resp.json()
-        result = data['data']['subscribers']
-        return (result)
-    else:
-        return (0)
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
